c8cee2d2f7cc5f335b02f619b58d828394ecc219
Roger <rogernuica3@gmail.com>
2021-01-24 19:02:31 +0200

    asynchronous methods

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace DatingApp.API.Controllers
@@ -22,17 +23,17 @@ namespace DatingApp.API.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetValues(){
-            var values = _context.Values.ToList();
+        public async Task<IActionResult> GetValues(){
+            var values = await _context.Values.ToListAsync();
 
             return Ok(values);
         }
 
           // GET api/values/5
         [HttpGet("{id}")]
-        public IActionResult GetValue(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            var value =  _context.Values.FirstOrDefault(x => x.Id == id);
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
 
             return Ok(value);
         }
