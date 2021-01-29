734c1032918d31cde76af38a4be56650a1cc73af
Roger <rogernuica3@gmail.com>
2021-01-29 16:07:52 +0200

    login service

---
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { logging } from 'protractor';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+constructor(private http: HttpClient) {}
+
+  baseUrl = 'http://localhost:5000/api/auth/';
+
+  login(model: any): any {
+return this.http.post(this.baseUrl + 'login', model)
+.pipe(
+  map((response: any) => {
+    const user = response;
+    if (user){
+      localStorage.setItem('token', user.token);
+     }
+   })
+  );
+ }
+}
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  model: any = {};
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  login(): void {
+    console.log(this.model);
   }
 
 }
