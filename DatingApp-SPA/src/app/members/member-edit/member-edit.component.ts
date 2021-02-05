2193412aa7e62043b3833a9d0a7d355925cf597a
Roger <rogernuica3@gmail.com>
2021-02-05 16:04:10 +0200

    maintain the changes after editing

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/_models/user';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -9,13 +11,19 @@ import { User } from 'src/app/_models/user';
 })
 export class MemberEditComponent implements OnInit {
   user: User;
+  @ViewChild('editForm', {static: true}) editForm: NgForm;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private alertify: AlertifyService) { }
 
   ngOnInit(): any {
     this.route.data.subscribe(data => {
       this.user = data.user;
     });
   }
+  updateUser(): any {
 
+   this.alertify.success('Profile updated succesfully');
+   this.editForm.reset(this.user); // gets back the dirty form state
+ }
 }
