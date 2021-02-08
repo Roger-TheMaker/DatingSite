b11efb07a75fcf0aea7186d00aa0e05167e56476
Roger <rogernuica3@gmail.com>
2021-02-08 18:18:28 +0200

    SPA pagination user service and more

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -17,7 +17,7 @@ export class MemberListComponent implements OnInit {
 
   ngOnInit(): any{
     this.route.data.subscribe( data => {
-      this.users = data.users;
+      this.users = data.users.result;
     });
   }
 
