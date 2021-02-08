96f0d45df6c5fa11e298aedb731f19db6b29f036
Roger <rogernuica3@gmail.com>
2021-02-08 14:15:13 +0200

    object assign register and Dtos modifications

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { User } from '../_models/user';
 import { AlertifyService } from '../_services/alertify.service';
 import { AuthService } from '../_services/auth.service';
 
@@ -11,13 +13,14 @@ import { AuthService } from '../_services/auth.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  user: User;
   registerForm: FormGroup;
   bsConfig: Partial<BsDatepickerConfig>;
 
   constructor(private authService: AuthService,
               private alertify: AlertifyService,
-              private fb: FormBuilder) { }
+              private fb: FormBuilder,
+              private router: Router) { }
 
   ngOnInit(): any {
     this.bsConfig = {
@@ -44,14 +47,20 @@ export class RegisterComponent implements OnInit {
     return g.get('password').value === g.get('confirmPassword').value ? null : {'mismatch': true};
   }
 
-  register(): void {
-    // this.authService.register(this.model).subscribe(() => {
-    //   this.alertify.success('registration sucessful');
-    // }, error => {
-    //   this.alertify.error('The fields are required for completion');
-    //   console.log(error);
-    // });
-    console.log(this.registerForm.value);
+  register(): any {
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      // clones the values into an empty object
+      this.authService.register(this.user).subscribe(() => {
+      this.alertify.success('Registration successful');
+        }, error => {
+         this.alertify.error(error);
+        }, () => {
+          this.authService.login(this.user).subscribe(() => {
+            this.router.navigate(['/members']);
+          });
+        });
+     }
   }
 
   cancel(): void{
