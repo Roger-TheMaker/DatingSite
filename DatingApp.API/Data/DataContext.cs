51dc8811e8ca5c6be4a20fd72bfc3994be6a6720
Roger <rogernuica3@gmail.com>
2021-02-13 19:43:10 +0200

    likes many to many integration

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -11,5 +11,28 @@ namespace DatingApp.API.Data
         public DbSet<User> Users {get; set; }
 
         public DbSet<Photo> Photos { get; set; }
+
+        public DbSet<Like> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Like>()
+                .HasKey(k => new {k.LikerId, k.LikeeId});
+                //pentru a avea maxim un like de la o poersoana la alta
+
+            builder.Entity<Like>()
+                .HasOne(u => u.Likee)
+                .WithMany(u => u.Likers)
+                .HasForeignKey(u => u.LikeeId)
+                .OnDelete(DeleteBehavior.Restrict);
+                //no cascade deletion
+
+            builder.Entity<Like>()
+                .HasOne(u => u.Liker)
+                .WithMany(u => u.Likees)
+                .HasForeignKey(u => u.LikerId)
+                .OnDelete(DeleteBehavior.Restrict);
+            //many to mony relationship
+        }
     }
 }
