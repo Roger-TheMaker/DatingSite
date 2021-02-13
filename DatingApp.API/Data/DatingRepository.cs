7f69df4cfb9507aa0816f387bc6495d12cb85d26
Roger <rogernuica3@gmail.com>
2021-02-13 20:13:57 +0200

    likers and likees API

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -26,6 +26,12 @@ namespace DatingApp.API.Data
             _context.Remove(entity);
         }
 
+        public async Task<Like> GetLike(int userId, int recipientId)
+        {
+            return await _context.Likes.FirstOrDefaultAsync(u => 
+                u.LikerId == userId && u.LikeeId == recipientId);
+        }
+
         public async Task<Photo> GetMainPhotoForUser(int userId)
         {
             return  await _context.Photos.Where(u => u.UserId == userId)
@@ -44,6 +50,25 @@ namespace DatingApp.API.Data
             .FirstOrDefaultAsync( u => u.Id == id);
             return user;
         }
+        
+         private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
+        {   // bool -> true -> the users that liked the current logged in user, 
+            // bool -> false -> the users that I have been liked
+            var user = await _context.Users
+                .Include(x => x.Likers)
+                .Include(x => x.Likees)
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            if (likers)
+            {
+               return user.Likers.Where(u => u.LikeeId == id).Select(i => i.LikerId);
+               // select -> returns a list of integers, not users
+            }
+            else 
+            {
+               return user.Likees.Where(u => u.LikerId == id).Select(i => i.LikeeId);
+            }
+        }
 
         public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
@@ -54,6 +79,19 @@ namespace DatingApp.API.Data
             users = users.Where(u => u.Gender == userParams.Gender);
             //returnam doar utilizatorii cu genderul setat din userParams,
             //care este pe dos acolo in functie de genderul nostru
+
+            if(userParams.Likers)
+            {
+                var userLikers = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikers.Contains(u.Id));
+            }
+
+            if(userParams.Likees)
+            {
+                var userLikees = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikees.Contains(u.Id));
+            }
+
 
             if(userParams.MinAge != 18 || userParams.MaxAge != 99 ){
 
